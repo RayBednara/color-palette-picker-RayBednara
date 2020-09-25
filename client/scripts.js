832df305e1aeab994c9285ae76d3a9dc965d4da5
raybednara <ray.bednara@att.net>
2020-09-24 20:25:35 -0500

    update child divs to append and remove greyscale when clicked

--- client/scripts.js
+++ client/scripts.js
@@ -2,6 +2,13 @@ const display = document.querySelector("main > div");
 const pallets = document.querySelectorAll("div > div");
 const compStyle = getComputedStyle(pallets[0]);
 
+const resetPalette = () => {
+  const greyed = document.querySelector(".greyscale");
+  if (greyed) {
+    greyed.classList.remove("greyscale");
+  }
+};
+
 pallets.forEach((colorPalette, index) => {
   colorPalette.style.setProperty(
     "background-color",
@@ -9,9 +16,12 @@ pallets.forEach((colorPalette, index) => {
   );
 
   colorPalette.addEventListener("click", () => {
+    resetPalette();
     display.style.setProperty(
       "background-color",
       colorPalette.style.getPropertyValue("background-color")
     );
+
+    colorPalette.classList.add("greyscale");
   });
 });
