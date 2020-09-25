46ced9620cbd0a5b52d1d7c8e7122ec27a1b2d4b
raybednara <ray.bednara@att.net>
2020-09-24 20:11:17 -0500

    add click event to handle color changing

--- client/scripts.js
+++ client/scripts.js
@@ -1,11 +1,17 @@
 const display = document.querySelector("main > div");
 const pallets = document.querySelectorAll("div > div");
+const compStyle = getComputedStyle(pallets[0]);
 
-const compStyle = getComputedStyle(document.querySelector("div > div"));
-console.log(compStyle);
 pallets.forEach((colorPalette, index) => {
   colorPalette.style.setProperty(
     "background-color",
     compStyle.getPropertyValue(`--${index}`)
   );
+
+  colorPalette.addEventListener("click", () => {
+    display.style.setProperty(
+      "background-color",
+      colorPalette.style.getPropertyValue("background-color")
+    );
+  });
 });
